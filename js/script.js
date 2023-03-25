8e59c38342807043e92f7625397d2cd16859e63c
Artem Yunkevich <777beezy@gmail.com>
2023-03-25 19:22:49 +0300

    first field added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log("start");
+
+const fieldElement = document.querySelector(".field");
+const infoElement = document.querySelector(".info");
+
+infoElement.innerHTML = "456";
+
+const field = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+fieldElement.innerHTML = 
+
+field.reduce((acc, y) => {
+    acc += y.join(" ") + "\n";
+    
+    return acc;
+}, "<pre>") + "</pre>";
